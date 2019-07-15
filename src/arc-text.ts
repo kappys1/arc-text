dd9836bf71959e0619b76d88923a11f73ff0c5f3
alex marcos <alexsbd1@gmail.com>
2019-07-15 10:07:20 +0200

    fix(lint): solve lint errors

--- src/arc-text.ts
+++ src/arc-text.ts
@@ -1,21 +1,20 @@
-import { IRect } from './interfaces';
-import { splitNode, getRect, chord, sagitta, getLetterRotations } from "./utils";
+import { IRect } from './interfaces'
+import { splitNode, getRect, chord, sagitta, getLetterRotations } from './utils'
 
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
 
-const { PI, max, min } = Math;
+const { PI, max, min } = Math
 
 export default class ArcText {
+  private element: HTMLElement
+  private originalHTML: string
+  private container: HTMLElement
+  public letters: HTMLElement[]
 
-  private element: HTMLElement;
-  private originalHTML: string;
-  private container: HTMLElement;
-  public letters: HTMLElement[];
-
-  private fontSize: number;
-  private lineHeight: number;
+  private fontSize: number
+  private lineHeight: number
   private metrics: IRect[]
   private minRadius: number
   private isForceWidth: boolean
@@ -25,63 +24,62 @@ export default class ArcText {
   private raf: any
 
   constructor(elm: HTMLElement, splitter: any) {
-    this.element = elm;
-    this.originalHTML = this.element.innerHTML;
+    this.element = elm
+    this.originalHTML = this.element.innerHTML
 
-    const container = document.createElement('div');
-    const fragment = document.createDocumentFragment();
-    container.setAttribute('aria-label', this.element.innerText);
-    container.style.position = 'relative';
-    this.container = container;
+    const container = document.createElement('div')
+    const fragment = document.createDocumentFragment()
+    container.setAttribute('aria-label', this.element.innerText)
+    container.style.position = 'relative'
+    this.container = container
 
-    this.letters = splitNode(this.element, splitter);
-    this.letters.forEach((letter: any) => fragment.appendChild(letter));
-    container.appendChild(fragment);
+    this.letters = splitNode(this.element, splitter)
+    this.letters.forEach((letter: any) => fragment.appendChild(letter))
+    container.appendChild(fragment)
 
-    this.element.innerHTML = '';
-    this.element.appendChild(container);
+    this.element.innerHTML = ''
+    this.element.appendChild(container)
 
-    const { fontSize, lineHeight }: any = window.getComputedStyle(this.element);
+    const { fontSize, lineHeight }: any = window.getComputedStyle(this.element)
 
-    this.fontSize = parseFloat(fontSize);
-    this.lineHeight = parseFloat(lineHeight) || this.fontSize;
-    this.metrics = this.letters.map(getRect);
+    this.fontSize = parseFloat(fontSize)
+    this.lineHeight = parseFloat(lineHeight) || this.fontSize
+    this.metrics = this.letters.map(getRect)
 
-    const totalWidth = this.metrics.reduce((sum: any, { width }: IRect) => sum + width, 0);
-    this.minRadius = (totalWidth / PI / 2) + this.lineHeight;
+    const totalWidth = this.metrics.reduce((sum: any, { width }: IRect) => sum + width, 0)
+    this.minRadius = totalWidth / PI / 2 + this.lineHeight
 
-    this.dir = 1;
-    this.isForceWidth = false;
-    this.isForceHeight = true;
-    this.radius = this.minRadius;
+    this.dir = 1
+    this.isForceWidth = false
+    this.isForceHeight = true
+    this.radius = this.minRadius
 
-    this.invalidate();
+    this.invalidate()
   }
 
   public forceWidth(value: boolean) {
-    if (value !== undefined) {
-      this.isForceWidth = value;
+    if (value) {
+      this.isForceWidth = value
 
-      this.invalidate();
+      this.invalidate()
 
-      return this;
+      return this
     }
 
-    return this.isForceWidth;
+    return this.isForceWidth
   }
 
   public forceHeight(value: boolean) {
-    if (value !== undefined) {
-      this.isForceHeight = value;
+    if (value) {
+      this.isForceHeight = value
 
-      this.invalidate();
+      this.invalidate()
 
-      return this;
+      return this
     }
 
-    return this.isForceHeight;
+    return this.isForceHeight
   }
-
 
   /**
    * Sets the text direction. `1` is clockwise, `-1` is counter-clockwise.
@@ -103,12 +101,12 @@ export default class ArcText {
    * arcText.dir(1);
    */
   public direction(value: number) {
-    if (value !== undefined) {
-      this.dir = value;
-      this.invalidate();
-      return this;
+    if (value) {
+      this.dir = value
+      this.invalidate()
+      return this
     }
-    return this.dir;
+    return this.dir
   }
 
   /**
@@ -128,15 +126,14 @@ export default class ArcText {
    * //=> 150
    */
   public arc(value: number) {
-    if (value !== undefined) {
-      this.radius = max(this.minRadius, value);
-      this.invalidate();
-      return this;
+    if (value) {
+      this.radius = max(this.minRadius, value)
+      this.invalidate()
+      return this
     }
 
-    return this.radius;
+    return this.radius
   }
-
 
   /**
    * Removes the ArcText effect from the element, restoring it to its
@@ -151,8 +148,8 @@ export default class ArcText {
    * arcText.destroy();
    */
   public destroy() {
-    this.element.innerHTML = this.originalHTML;
-    return this;
+    this.element.innerHTML = this.originalHTML
+    return this
   }
 
   /**
@@ -168,9 +165,8 @@ export default class ArcText {
    * arcText.destroy();
    */
   public refresh() {
-    return this.invalidate();
+    return this.invalidate()
   }
-
 
   /**
    * Invalidates the current state and schedules a task to refresh the layout
@@ -182,15 +178,14 @@ export default class ArcText {
    */
   private invalidate() {
     // if(this.raf) {
-      cancelAnimationFrame(this.raf);
+    cancelAnimationFrame(this.raf)
     // }
     this.raf = requestAnimationFrame(() => {
-      this.render();
-    });
+      this.render()
+    })
 
-    return this;
+    return this
   }
-
 
   /**
    * Rotates and positions the letters.
@@ -200,39 +195,38 @@ export default class ArcText {
    * @return {ArcText} This instance.
    */
   private render() {
-    const originY = this.dir === -1 ? (-this.radius + this.lineHeight) : this.radius ;
-    const origin = `center ${originY / this.fontSize}em`;
-    const innerRadius = this.radius  - this.lineHeight;
-    const { rotations, θ } = getLetterRotations(this.metrics, innerRadius);
+    const originY = this.dir === -1 ? -this.radius + this.lineHeight : this.radius
+    const origin = `center ${originY / this.fontSize}em`
+    const innerRadius = this.radius - this.lineHeight
+    const { rotations, θ } = getLetterRotations(this.metrics, innerRadius)
 
     this.letters.forEach((letter, index) => {
-      const { style } = letter;
-      const rotate = ((θ * -0.5) + rotations[index]) * this.dir;
-      const translateX = (this.metrics[index].width * -0.5) / this.fontSize;
-      const transform = `translateX(${translateX}em) rotate(${rotate}deg)`;
+      const { style } = letter
+      const rotate = (θ * -0.5 + rotations[index]) * this.dir
+      const translateX = (this.metrics[index].width * -0.5) / this.fontSize
+      const transform = `translateX(${translateX}em) rotate(${rotate}deg)`
 
-      style.position = 'absolute';
-      style.bottom = this.dir === -1 ? '0': 'auto';
-      style.left = '50%';
-      style.transform = transform;
-      style.transformOrigin = origin;
-      style.webkitTransform = transform;
-      style.webkitTransformOrigin = origin;
-    });
+      style.position = 'absolute'
+      style.bottom = this.dir === -1 ? '0' : 'auto'
+      style.left = '50%'
+      style.transform = transform
+      style.transformOrigin = origin
+      // style.webkitTransform = transform;
+      // style.webkitTransformOrigin = origin;
+    })
 
     if (this.isForceHeight) {
-      const height = θ > 180 ? sagitta(this.radius , θ) : sagitta(innerRadius, θ) + this.lineHeight;
+      const height = θ > 180 ? sagitta(this.radius, θ) : sagitta(innerRadius, θ) + this.lineHeight
 
-      this.container.style.height = `${height / this.fontSize}em`;
+      this.container.style.height = `${height / this.fontSize}em`
     }
 
     if (this.isForceWidth) {
-      const width = chord(this.radius , min(180, θ));
+      const width = chord(this.radius, min(180, θ))
 
-      this.container.style.width = `${width / this.fontSize}em`;
+      this.container.style.width = `${width / this.fontSize}em`
     }
 
-    return this;
+    return this
   }
-
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,37 +1,40 @@
-import { IRect } from './interfaces';
+import { IRect } from './interfaces'
 
+export const splitNode = (node: HTMLElement, splitter: any) => {
+  const wrapperElement = document.createElement('span')
+  const text: string = node.innerText.trim()
+  const chars = splitter ? splitter(text) : [...text.split('')]
+  return chars.map((char: string) => {
+    const parent: any = wrapperElement.cloneNode()
+    parent.insertAdjacentHTML('afterbegin', char === ' ' ? '&nbsp;' : char)
+    return parent
+  })
+}
 
-export const splitNode =  (node: HTMLElement, splitter: any) => {
-  const wrapperElement = document.createElement('span');
-  const text: string = node.innerText.trim();
-  const chars = splitter ? splitter(text) : [ ...text.split('') ];
-  return chars.map((char:string) => {
-    const parent: any = wrapperElement.cloneNode();
-    parent.insertAdjacentHTML('afterbegin', char === ' ' ? '&nbsp;' : char);getLetterRotations
-    return parent;
-  });
-};
-
-export const getRect = (element: HTMLElement) : IRect => {
-  const rect = element.getBoundingClientRect();
+export const getRect = (element: HTMLElement): IRect => {
+  const rect = element.getBoundingClientRect()
   return {
     height: rect.height,
     left: rect.left + window.pageXOffset,
     top: rect.top + window.pageYOffset,
-    width: rect.width,
-  };
-};
+    width: rect.width
+  }
+}
 
-const radiansPerDegree = Math.PI / 180;
-const degreesPerRadian = 180 / Math.PI;
-export const chord = (r: number, θ: any) => 2 * r * Math.sin(degreesToRadians(θ / 2));
-export const radiansToDegrees = (angleInDegrees: number) => angleInDegrees * degreesPerRadian;
-export const degreesToRadians = (angleInDegrees: number) => angleInDegrees * radiansPerDegree;
-export const sagitta = (r: number, θ: any) => r * (1 - Math.cos(degreesToRadians(θ / 2)));
-export const getLetterRotations = (metrics: any, r: number) => metrics.reduce((data: any, { width } : any) => {
-  const rotation = radiansToDegrees(width / r);
-  return {
-    θ: data.θ + rotation,
-    rotations: data.rotations.concat([ data.θ + (rotation / 2) ]),
-  };
-}, { θ: 0, rotations: [] });
+const radiansPerDegree = Math.PI / 180
+const degreesPerRadian = 180 / Math.PI
+export const chord = (r: number, θ: any) => 2 * r * Math.sin(degreesToRadians(θ / 2))
+export const radiansToDegrees = (angleInDegrees: number) => angleInDegrees * degreesPerRadian
+export const degreesToRadians = (angleInDegrees: number) => angleInDegrees * radiansPerDegree
+export const sagitta = (r: number, θ: any) => r * (1 - Math.cos(degreesToRadians(θ / 2)))
+export const getLetterRotations = (metrics: any, r: number) =>
+  metrics.reduce(
+    (data: any, { width }: any) => {
+      const rotation = radiansToDegrees(width / r)
+      return {
+        θ: data.θ + rotation,
+        rotations: data.rotations.concat([data.θ + rotation / 2])
+      }
+    },
+    { θ: 0, rotations: [] }
+  )
